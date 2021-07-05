e3c98268a156acb045c70d846633e3161263c797
VinayDasari07 <vinay.dasari.07@gmail.com>
2021-07-05 17:35:43 +0530

    Added Footer and FAQs

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { FaqsContainer } from '../containers/faqs';
 import { JumbotronContainer } from '../containers/jumbotron';
+import { FooterContainer } from '../containers/footer';
 
 export default function Home() {
     return (
-        <JumbotronContainer />        
+        <>
+            <JumbotronContainer />
+            <FaqsContainer />
+            <FooterContainer />
+        </>
     )
 }
