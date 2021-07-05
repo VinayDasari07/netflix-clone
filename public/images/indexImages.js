929d382d533d9199f8c9b43d8b94f852c161f71f
VinayDasari07 <vinay.dasari.07@gmail.com>
2021-07-05 20:10:43 +0530

    Added Header and feature component

--- public/images/indexImages.js
+++
@@ -1,3 +0,0 @@
-export const HomeTV = require('./misc/home-tv.jpg');
-export const HomeMob = require('./misc/home-mobile.jpg');
-export const HomeiMac = require('./misc/home-imac.jpg');
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 import faqsData from '../fixtures/faqs.json'
 
 export function FaqsContainer() {
@@ -14,5 +14,14 @@ export function FaqsContainer() {
                     </Accordion.Item>
                 ))}
             </Accordion.Frame>
-        </Accordion>);
+            
+            <OptForm>
+                <OptForm.Input placeholder="Email Address" />
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Break />
+                <OptForm.Text>Ready to watch? Enter your email to create or restart
+                your membership.</OptForm.Text>
+            </OptForm>
+        </Accordion>
+    );
 }
